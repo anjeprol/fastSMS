c906d034e82fa11f14977b370f7f2bcf431aa5f7
anjeprol <anjeprol_prado@hotmail.com>
2016-03-28 17:31:30 -0400

    New version with material design, internationalization and validations

--- app/src/main/java/com/prolan/fastsms/MainActivity.java
+++ app/src/main/java/com/prolan/fastsms/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -17,9 +18,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
-    Button sendBtn;
-    EditText txtphoneNo;
-    EditText txtMessage;
+    private Button sendBtn;
+    private EditText txtphoneNo;
+    private EditText txtMessage;
+    private TextInputLayout inputNumber, inputMessage ;
 
 
     @Override
@@ -27,9 +29,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Loading views
         sendBtn = (Button) findViewById(R.id.btnSendSMS);
         txtphoneNo = (EditText) findViewById(R.id.editText);
         txtMessage = (EditText) findViewById(R.id.editText2);
+        inputMessage = (TextInputLayout) findViewById(R.id.input_layout_textMessage);
+        inputNumber = (TextInputLayout) findViewById(R.id.input_layout_textPhone);
+
+
 
         //Reading the last phone number used
         SharedPreferences prefs = getSharedPreferences("myLstPhone", Context.MODE_PRIVATE);
@@ -70,19 +77,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void sendSMS(){
+        String labelNumber = getString(R.string.need_a_number);
+        String labelMessage = getString(R.string.need_a_message);
+        String errLabelNum = getString(R.string.err_number);
+        String errLabelMsg = getString(R.string.err_messageSMS);
         Log.d("Send msg","");
         String phonNum = txtphoneNo.getText().toString();
         String txtSMS = txtMessage.getText().toString();
-        if(phonNum.isEmpty() )
-            Toast.makeText(getApplicationContext(),"I need a number =)", Toast.LENGTH_LONG).show();
-        else if (txtSMS.trim().isEmpty())
-            Toast.makeText(getApplicationContext(),"Nothing to say ? I need a message =)", Toast.LENGTH_LONG).show();
+        if(phonNum.trim().isEmpty() ){
+            Toast.makeText(getApplicationContext(),labelNumber, Toast.LENGTH_LONG).show();
+            inputNumber.setError(errLabelNum);
+            inputMessage.setErrorEnabled(false);
+            txtphoneNo.requestFocus();
+        }
+        else if (txtSMS.trim().isEmpty()){
+            Toast.makeText(getApplicationContext(),labelMessage, Toast.LENGTH_LONG).show();
+            inputMessage.setError(errLabelMsg);
+            txtMessage.requestFocus();
+            inputNumber.setErrorEnabled(false);
+        }
         else{
             try {
                 SmsManager smsManager = SmsManager.getDefault();
                 smsManager.sendTextMessage(phonNum,null,txtSMS,null,null);
                 Toast.makeText(getApplicationContext(),"SMS Sent", Toast.LENGTH_LONG).show();
                 txtMessage.setText("");
+                inputMessage.setErrorEnabled(false);
+                inputNumber.setErrorEnabled(false);
                 Log.d("Finished sending SMS...","");
             }catch (android.content.ActivityNotFoundException ex){
                 Toast.makeText(MainActivity.this,"SMS failed, please try again later", Toast.LENGTH_SHORT).show();
